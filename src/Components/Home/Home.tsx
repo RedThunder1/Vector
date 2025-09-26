99bbd4942ec522a33781308265ea6d4385da47dd
James <redthunderspartan@gmail.com>
2025-09-25 22:27:07 -0500

    Made a quick home page to help layout how I want it to look. (God I hate css)

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -3,9 +3,27 @@ import "./Home.css"
 
 function Home() {
     return (
-        <div className="home">
+        <>
+            <div className="home">
+                <div className="title_container">
+                    <h1 className="title">Welcome to Vector</h1>
+                    <p>Your go to for Tasks and Notes!</p>
+                </div>
+                <div className="quick_start">
+                    <div className="quick_list">
+                        <h3>Create a Todo List</h3>
+                        <p>Create a Todo List to track and manage tasks.</p>
+                        <button className="quick_button" type="button">New Todo List</button>
+                    </div>
+                    <div className="quick_note">
+                        <h3>Create a Note</h3>
+                        <p>Create a new Note to write down<br/>and save anything you want.</p>
+                        <button className="quick_button" type="button">New Note</button>
+                    </div>
+                </div>
+            </div>
+        </>
 
-        </div>
     )
 }
 export default Home
