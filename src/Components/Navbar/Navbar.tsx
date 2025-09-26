301bd2b88238755f0a926f510c2b3b10f36267a2
James <redthunderspartan@gmail.com>
2025-09-25 22:57:50 -0500

    Added Simple and maybe temp logo.

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 import react from 'react'
 import "./Navbar.css"
+import Logo from '../../Graphics/VectorLogo.png'
 
 function Navbar() {
     return (
         <div className="navbar">
+            <img src={Logo} alt="Vector Logo"></img>
             <div className="home_tab navbar_tab">Home</div>
             <div className="lists_tab navbar_tab">Todo Lists</div>
             <div className="notes_tab navbar_tab">Notes</div>
