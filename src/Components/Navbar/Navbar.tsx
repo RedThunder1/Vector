467fbaef3afd2d3c3e01fcf8702d618e13ff7e44
James <redthunderspartan@gmail.com>
2025-09-25 22:43:57 -0500

    Made a Nav bar to help layout how I want it to look.

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -4,6 +4,13 @@ import "./Navbar.css"
 function Navbar() {
     return (
         <div className="navbar">
+            <div className="home_tab navbar_tab">Home</div>
+            <div className="lists_tab navbar_tab">Todo Lists</div>
+            <div className="notes_tab navbar_tab">Notes</div>
+            <div className="utility_tabs">
+                <div className="account_tab">Account</div>
+                <div className="settings_tab">Settings</div>
+            </div>
 
         </div>
     )
