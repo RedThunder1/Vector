6da886275dfa3cc951734896fad9b21755955fad
RedThunder1 <redthunderspartan@gmail.com>
2025-10-19 20:26:49 -0500

    Some Changes.

---
+++ src/Components/List/TodoItem/TodoItem.tsx
@@ -0,0 +1,12 @@
+import react from "react";
+import './TodoItem.css'
+
+function TodoItem() {
+    return (
+        <div className="todoitem">
+
+        </div>
+    )
+}
+
+export default TodoItem
