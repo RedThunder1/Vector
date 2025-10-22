e714bf5eb51664bf5e47c7b3c27d86ee96b04848
RedThunder1 <redthunderspartan@gmail.com>
2025-10-22 13:12:33 -0500

    Did some work on creating tasks and sections.

--- src/Components/Account/Account.tsx
+++ src/Components/Account/Account.tsx
@@ -7,6 +7,7 @@ import Cookies from "universal-cookie";
 function Account() {
     const [identifier, setIdentifier] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         try {
@@ -19,7 +20,7 @@ function Account() {
             console.log(response)
         } catch (error) {
             //Show error on screen
-
+            document.getElementById("error_header")!!.style.display = "block"
             console.error('Failed to submit login data', error)
         }
     }
@@ -30,9 +31,12 @@ function Account() {
                 <form className="account_form" id="account_form" method="POST" onSubmit={(e) => {handleSubmit(e)}}>
 
                     <h2>Account Login</h2>
+                    <div className="error_header" id="error_header">
+                        There was an error while trying to login! Please try again!
+                    </div>
                     <input id="identifier"
                            value={identifier}
-                           onChange={(e) => setIdentifier(e.target.value)}
+                           onChange={(e) => setIdentifier(e.target.value) }
                            name="identifier"
                            className="account_identifier"
                            placeholder="Username or Email" required />
--- src/Components/List/List.tsx
+++ src/Components/List/List.tsx
@@ -1,19 +1,24 @@
-import react from "react"
+import React from "react"
 import './List.css'
+import {createPortal} from "react-dom";
+import {render} from "@testing-library/react";
+import ReactDOM from "react-dom/client";
 
 function List() {
+
     return (
-        <div className="list">
+        <div className="list" id="list">
             <div className='list_toolbar'>
                 <h2>ToolBar</h2>
                 <input id='list_title' type='text' placeholder='List Title'/>
+                <div className="new_section_button" onClick={() => {openSectionCreationPanel()}}>New Section</div>
             </div>
 
             {//Placeholder for Dev
             }
-            <div className='list_container'>
+            <div className='list_container' id='list_container'>
                 <div className='list_section'>
-                    <div className='list_section_header'>Section Header</div>
+                    <div className='list_section_header'><h3>Section Header</h3> <div className="new_task_button" onClick={(e) => {openTaskCreationPanel(e)}}>New</div> </div>
                     <div className='list_section_items'>
                         <div className='list_section_item'>
                             <div className='list_item_title'>
@@ -31,5 +36,84 @@ function List() {
         </div>
     )
 }
+
+function saveList() {
+
+}
+
+function loadList() {
+
+}
+
+function createTask(section: HTMLDivElement, name: string, description: string) {
+    document.getElementById("create_panel")!!.remove()
+    const task = (
+        <div className='list_section_item'>
+            <div className='list_item_title'>
+                {name}
+                <div className='remove_list_item'>X</div>
+            </div>
+            <div className='list_item_desc'>{description}</div>
+            <div className='list_item_tags'>
+            </div>
+        </div>
+    );
+
+    render(createPortal(task, section));
+}
+
+function openTaskCreationPanel(e: React.MouseEvent<HTMLDivElement>) {
+    const section = ((e.target as HTMLDivElement).parentElement as HTMLDivElement).parentElement as HTMLDivElement;
+    const list_element = document.getElementById('list') as HTMLDivElement;
+
+    const create_panel = (<div className="create_panel" id="create_panel">
+                            <h2>Create New Task</h2>
+                            <div className="create_panel_items">
+                              <input className="create_name" id="task_name" type="text" placeholder="Task Name"/>
+                              <textarea className="task_desc" id="task_desc" placeholder="Task Description"></textarea>
+                              <div className="create_button" onClick={() => {createTask(section, (document.getElementById("task_name") as HTMLInputElement).value, (document.getElementById("task_desc") as HTMLInputElement).value )}}>Create Task</div>
+                            </div>
+                          </div>
+    );
+    render(createPortal(create_panel, list_element));
+
+}
+
+
+
+function deleteListItem() {
+
+}
+
+function createListSection(name: string) {
+    document.getElementById("create_panel")!!.remove()
+    const section_container = document.getElementById('list_container') as HTMLDivElement;
+    const list_section = (
+        <div className='list_section' id='list_section'>
+            <div className='list_section_header'><h3>{name}</h3> <div className="new_task_button" onClick={(e) => {openTaskCreationPanel(e)}}>New</div> </div>
+            <div className='list_section_items'>
+            </div>
+        </div>
+    );
+    render(createPortal(list_section, section_container));
+}
+
+function openSectionCreationPanel() {
+    const list_element = document.getElementById('list') as HTMLDivElement;
+    const create_panel = (<div className="create_panel" id="create_panel">
+            <h2>Enter Section Name</h2>
+            <div className="create_panel_items">
+                <input className="create_name" id="section_name" type="text" placeholder="Section Name"/>
+                <div className="create_button" onClick={() => {createListSection((document.getElementById("section_name") as HTMLInputElement).value)}}>Create Task</div>
+            </div>
+        </div>
+    );
+    render(createPortal(create_panel, list_element));
+}
+
+function deleteListSection() {
+
+}
+
 
 export default List
